956918929b7822665838a389e0862bcca9ab46e9
肖伟 <1097526615@qq.com>
2017-02-19 16:08:27 +0800

    First Commit

---
+++ Weak_RuntimeImp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weak_RuntimeImp
+//
+//  Created by Metallic  on 17/2/19.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weak_RuntimeImp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Weak_RuntimeImp
+//
+//  Created by Metallic  on 17/2/19.
+//  Copyright © 2017年 xiaowei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
